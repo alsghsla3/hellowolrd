f412463384cb32ef55c361147ba38c5036c4dce1
alsgh <alsgh@118.32.239.136>
2019-02-13 22:22:27 +0900

    hi world 추가

--- src/hellowolrd/hellowolrd.java
+++ src/hellowolrd/hellowolrd.java
@@ -5,6 +5,7 @@ public class hellowolrd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello wolrd");
+		System.out.println("hi world");
 	}
 
 }
